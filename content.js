2aab875c675e2809fc036b47d0c8de76da652b0d
Qluxzz <andre.eric.andersson@gmail.com>
2025-01-22 18:06:29 +0100

    Add some error handling

--- content.js
+++ content.js
@@ -10,14 +10,22 @@ window.fetch = async (...args) => {
 
   if (typeof initOptions.body !== "string") return getOriginalResponse()
 
-  const requestBody = JSON.parse(initOptions.body)
+  try {
+    const requestBody = JSON.parse(initOptions.body)
 
-  if (requestBody.operationName !== "addToPlaylist")
+    if (requestBody.operationName !== "addToPlaylist")
+      return getOriginalResponse()
+
+    requestBody.variables.newPosition.moveType = "TOP_OF_PLAYLIST"
+
+    initOptions.body = JSON.stringify(requestBody)
+  } catch (error) {
+    console.error(
+      `requestBody "${initOptions.body}" couldn't be parsed or didn't have the expected key 'variables.newPosition.moveType'. This can happen if Spotify have updated their API. Please open an issue on https://github.com/qluxzz/spotify-add-to-top-of-playlist/issues/new\n${error}`
+    )
+
     return getOriginalResponse()
-
-  requestBody.variables.newPosition.moveType = "TOP_OF_PLAYLIST"
-
-  initOptions.body = JSON.stringify(requestBody)
+  }
 
   return _fetch(resource, initOptions)
 }
